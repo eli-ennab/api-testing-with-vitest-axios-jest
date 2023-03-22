f9ac22a8cd997797d837aaf49a2308bbd03d4379
Eli Ennab <elinaennab@gmail.com>
2023-03-22 15:52:34 +0100

    added generic get and post requests

--- src/services/BortakvallAPI.ts
+++ src/services/BortakvallAPI.ts
@@ -1,19 +1,47 @@
 import axios from 'axios'
+import { CreateProductData, Product } from '../types/BortakvallTypes'
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 /**
- * Get all products
+ * Generic get request
  */
-export const getProducts = async () => {
-	const res = await axios.get<any>(`${BASE_URL}/products`)
+export const get = async <T>(endpoint: string) => {
+	const res = await axios.get<T>(BASE_URL + endpoint)
 	return res.data
 }
+
+/**
+ * Generic post request
+ */
+export const post = async <T>(endpoint: string, data: any) => {
+	const res = await axios.post<T>(BASE_URL + endpoint, data)
+	return res.data
+}
+
+/**
+ * Get all products
+ */
+export const getProducts = () => {
+	return get<Product[]>(`/products`)
+}
+
+// export const getProducts = async () => {
+// 	const res = await axios.get<Product[]>(`${BASE_URL}/products`)
+// 	return res.data
+// }
 
 /**
  * Get a single product
  */
 export const getProduct = async (id: number) => {
-	const res = await axios.get<any>(`${BASE_URL}/products/${id}`)
+	const res = await axios.get<Product>(`${BASE_URL}/products/${id}`)
 	return res.data
+}
+
+/**
+ * Create a product
+ */
+export const createProduct = (product: CreateProductData) => {
+	return post<Product>(`/products`, product)
 }
