bd226e59d38c72d3970966df3c1769935468fe1d
Eli Ennab <elinaennab@gmail.com>
2023-03-22 15:52:51 +0100

    created types for product and order

--- src/types/BortakvallTypes.ts
+++ src/types/BortakvallTypes.ts
@@ -1,9 +1,48 @@
-/*
-export type Product {
-
+export type Product = {
+    status: string,
+    data: [
+        id: number,
+        name: string,
+        description: string,
+        price: number,
+        on_sale: boolean,
+        images: {
+        thumbnail: string,
+        large: string
+        },
+        stock_status: string,
+        stock_quantity: null
+    ]
 }
 
-export type Order {
+// export type Product = {
+// 	id: number,
+// 	name: string,
+// 	description: string,
+// 	price: number,
+// 	images: string,
+// 	stock_status: string,
+// 	stock_quantity: number,
+// }
 
+export type CreateProductData = {
+	name: string,
+	description: string,
+	price: number,
+	images: string,
+	stock_status: string,
+	stock_quantity: number,
 }
-*/
+
+export type Order = {
+	id: number,
+	customer_first_name: string,
+	customer_last_name: string,
+	customer_address: string,
+	customer_postcode: number,
+	customer_city: string,
+	customer_email: string,
+	customer_phone?: number | null,
+	order_total: number,
+}
+
