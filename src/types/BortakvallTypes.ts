dceab513bcfac6df48078542dac745925a0335fa
Eli Ennab <elinaennab@gmail.com>
2023-03-22 16:21:22 +0100

    types for order and commented out options on how to create types

--- src/types/BortakvallTypes.ts
+++ src/types/BortakvallTypes.ts
@@ -1,29 +1,32 @@
-export type Product = {
-    status: string,
-    data: [
-        id: number,
-        name: string,
-        description: string,
-        price: number,
-        on_sale: boolean,
-        images: {
-        thumbnail: string,
-        large: string
-        },
-        stock_status: string,
-        stock_quantity: null
-    ]
-}
-
 // export type Product = {
-// 	id: number,
-// 	name: string,
-// 	description: string,
-// 	price: number,
-// 	images: string,
-// 	stock_status: string,
-// 	stock_quantity: number,
+//     status: string,
+//     data: [
+//         id: number,
+//         name: string,
+//         description: string,
+//         price: number,
+//         on_sale: boolean,
+//         images: {
+//         thumbnail: string,
+//         large: string
+//         },
+//         stock_status: string,
+//         stock_quantity: null
+//     ]
 // }
+
+/**
+ * Product
+ */
+export type Product = {
+	id: number,
+	name: string,
+	description: string,
+	price: number,
+	images: string,
+	stock_status: string,
+	stock_quantity: number,
+}
 
 export type CreateProductData = {
 	name: string,
@@ -34,6 +37,9 @@ export type CreateProductData = {
 	stock_quantity: number,
 }
 
+/**
+ * Order
+ */
 export type Order = {
 	id: number,
 	customer_first_name: string,
@@ -45,4 +51,35 @@ export type Order = {
 	customer_phone?: number | null,
 	order_total: number,
 }
+
+export type CreateOrderData = {
+	customer_first_name: string,
+	customer_last_name: string,
+	customer_address: string,
+	customer_postcode: string,
+	customer_city: string,
+	customer_email: string,
+	customer_phone: string,
+	order_total: number,
+	order_items: [],
+}
+
+// export type CreateOrderData = {
+// 	customer_first_name: string,
+// 	customer_last_name: string,
+// 	customer_address: string,
+// 	customer_postcode: string,
+// 	customer_city: string,
+// 	customer_email: string,
+// 	customer_phone: string,
+// 	order_total: number,
+// 	order_items: [
+// 			{
+// 			product_id: number,
+// 			qty: number,
+// 			item_price: number,
+// 			item_total: number,
+// 		},
+// 	],
+// }
 
