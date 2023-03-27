45d2e8e4d25a2e78cce73edb5d2495f2389756f1
Eli Ennab <elinaennab@gmail.com>
2023-03-27 10:34:50 +0200

    implemented orderitem and cleaned code

--- src/tests/BortakvallAPI.test.ts
+++ src/tests/BortakvallAPI.test.ts
@@ -3,17 +3,14 @@ import { server } from '../mocks/server'
 import * as BortakvallAPI from '../services/BortakvallAPI'
 import { OrderData, ProductData } from '../types/BortakvallTypes'
 
-// Boot API mocking
 beforeAll(() => {
 	server.listen()
 })
 
-// Reset handlers
 afterEach(() => {
 	server.resetHandlers()
 })
 
-// Clean up after ourselves
 afterAll(() => {
 	server.close()
 })
@@ -23,8 +20,8 @@ const newProduct: ProductData = {
 	description: "<p>Vingummi med colasmak</p>\n<p>Innehållsförteckning: Glukossirap, socker, gelatin, druvsocker, syra: citronsyra, karamelliserat socker, arom, palmolja, ytbehandlingsmedel: bivax vitt och gult, karnaubavax.</p>\n<p>Kan innehålla VETE.</p>\n<p><em>Alla priser är per skopa.</em></p>\n",
 	price: 7,
 	images: {
-		large: "https://www.boredpanda.com/blog/wp-content/uploads/2022/09/relatable-funny-memes-22-63284d45ebe28__700.jpg",
-		thumbnail: "https://www.boredpanda.com/blog/wp-content/uploads/2022/09/relatable-funny-memes-22-63284d45ebe28__700.jpg",
+		large: "https://http.cat/201",
+		thumbnail: "https://http.cat/201",
 	},
 	stock_status: "instock",
 	stock_quantity: 2
@@ -39,12 +36,22 @@ const newOrder: OrderData = {
 	customer_email: "scotts.totts@dundermifflin.org",
 	customer_phone: "0755-555555",
 	order_total: 1,
-	order_items: [],
+	order_items: [
+		{
+			id: 31,
+			order_id: 27,
+			product_id: 8,
+			qty: 1,
+			item_price: 12,
+			item_total: 12
+		}
+	]
 }
 
 describe('Product tests', () => {
 	it('should return a list of products', async () => {
 		const products = await BortakvallAPI.getProducts()
+
 		expect(Array.isArray(products.data)).toBe(true)
 		expect(products.data.length).toBeGreaterThan(0)
 		expect(products.status).toBe("success")
@@ -53,6 +60,7 @@ describe('Product tests', () => {
 
 	it('should create a product', async () => {
 		const createdProduct = await BortakvallAPI.createProduct(newProduct)
+
 		expect(createdProduct.data).toMatchObject({
 			id: expect.any(Number),
 			name: newProduct.name,
@@ -69,6 +77,7 @@ describe('Product tests', () => {
 	it('should create a product and then get that product', async () => {
 		const createdProduct = await BortakvallAPI.createProduct(newProduct)
 		const getProduct = await BortakvallAPI.getProduct(createdProduct.data.id)
+
 		expect(getProduct).toStrictEqual(createdProduct)
 		expect(getProduct.status).toBe("success")
 		expect(getProduct.status).not.toBe("error" || "fail")
@@ -80,6 +89,7 @@ describe('Product tests', () => {
 describe('Order tests', () => {
 	it('should return a list of orders', async () => {
 		const orders = await BortakvallAPI.getOrders()
+
 		expect(Array.isArray(orders.data)).toBe(true)
 		expect(orders.data.length).toBeGreaterThan(0)
 		expect(orders.status).toBe("success")
@@ -88,6 +98,7 @@ describe('Order tests', () => {
 
 	it('should create an order', async () => {
 		const createdOrder = await BortakvallAPI.createOrder(newOrder)
+
 		expect(createdOrder.data).toMatchObject({
 			id: expect.any(Number),
 			customer_first_name: newOrder.customer_first_name,
@@ -100,7 +111,6 @@ describe('Order tests', () => {
 			order_total: newOrder.order_total,
 			order_items: newOrder.order_items
 		})
-
 		expect(createdOrder.status).toBe("success")
 		expect(createdOrder.status).not.toBe("error" || "fail")
 	})
@@ -108,8 +118,8 @@ describe('Order tests', () => {
 	it('should create a order and then get that order', async () => {
 		const createdOrder = await BortakvallAPI.createOrder(newOrder)
 		const getOrder = await BortakvallAPI.getOrder(createdOrder.data.id)
-		expect(getOrder).toStrictEqual(createdOrder)
 
+		expect(getOrder).toStrictEqual(createdOrder)
 		expect(getOrder.status).toBe("success")
 		expect(getOrder.status).not.toBe("error" || "fail")
 		expect(createdOrder.status).toBe("success")
